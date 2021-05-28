d1827f043d51e05bb7e09e2c3a359aef06727468
AkaiGoblin <carrier.marcantoine@gmail.com>
2021-05-27 21:28:58 -0400

    Update Coin.controller, Button.controller, and 22 more files...

---
+++ Assets/Scripts/Controllers/ButtonController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+	private Animator _animator;
+	private Collider2D _boxCollider;
+
+	#region Delegates
+	public delegate void ButtonPushedHandler();
+	#endregion
+
+	#region Events
+	public event ButtonPushedHandler ButtonPushed;
+	#endregion
+
+	private void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		_boxCollider = GetComponent<BoxCollider2D>();
+	}
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.GetComponent<Ninja>() != null)
+		{
+			_animator.SetTrigger("ButtonPushed");
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.GetComponent<Ninja>() != null)
+		{
+			_animator.SetTrigger("ButtonPushed");
+			OnButtonPushed();
+		}		
+	}
+
+	private void OnButtonPushed()
+	{
+		if (ButtonPushed!=null)
+		{
+			ButtonPushed();
+		}
+	}
+}
